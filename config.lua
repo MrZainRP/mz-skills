6fd8bf341ae1740ca82d17881eee86ee44d6f0ce
Mister Zain <95512163+MrZainRP@users.noreply.github.com>
2024-07-20 15:49:01 +1000

    added "Hunting" skill

--- config.lua
+++ config.lua
@@ -114,6 +114,12 @@ Config.Skills = {
         ["Stat"] = "ELECTRICAL_ABILITY",
         ['icon'] = 'fas fa-bolt',
     }, 
+    ["Hunting"] = {
+        ["Current"] = 0,
+        ["RemoveAmount"] = 0,
+        ["Stat"] = "HUNTING_ABILITY",
+        ['icon'] = 'fas fa-paw',
+    },
 
 ---------------------
 --GTA NATIVE SKILLS-- UNSUPPORTED FUNCTIONALITY FROM THE ORIGINAL B1-SKILLZ
